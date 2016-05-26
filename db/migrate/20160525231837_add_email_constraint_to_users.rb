cf75a811c6157a4fb705d262ec6a21daffa9b962
Doh <tank2705@gmail.com>
2016-05-25 17:25:29 -0700

    Completed adding constraints to db

---
+++ db/migrate/20160525231837_add_email_constraint_to_users.rb
@@ -0,0 +1,20 @@
+class AddEmailConstraintToUsers < ActiveRecord::Migration[5.0]
+  def up
+  	execute %{
+		ALTER TABLE
+			users
+		ADD CONSTRAINT
+			email_must_be_company_email
+		CHECK ( email ~* '^[^@]+@ams-fa\\.com' )	
+  end
+
+  def down
+  	execute %{
+  		ALTER TABLE
+  			users
+  		DROP CONSTRAINT
+  			email_must_be_company_email
+  	}
+  end
+
+end
